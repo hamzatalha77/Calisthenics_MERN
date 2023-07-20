d09aa0ed59c82fff0b0b7c82166218d6571e9bcc
hamzatalha77 <hamza_ta7@hotmail.com>
2023-07-20 19:02:09 +0100

    commit

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,15 @@
-console.log('hello world')
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT
+
+app.get('/', (req, res) => {
+  res.send('Express + TypeScript Server')
+})
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`)
+})
