86ecebf5cfde4dba73a8690dc45fc74de89295ad
hamzatalha77 <hamza_ta7@hotmail.com>
2023-07-21 14:37:22 +0100

    commit

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
-
+import cors from 'cors'
+import mongoose from 'mongoose'
 dotenv.config()
-
-const app = express()
 const port = process.env.PORT
+const app = express()
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Express + TypeScript Server')
