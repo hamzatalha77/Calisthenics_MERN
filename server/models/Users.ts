0fe6308e6c56d7d77733bf2b794f25227ee16fd5
hamzatalha77 <hamza_ta7@hotmail.com>
2023-07-22 14:17:21 +0100

    commit

--- server/models/Users.ts
+++ server/models/Users.ts
@@ -1,5 +1,10 @@
 import mongoose from 'mongoose'
 
 const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true }
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  user_image: { type: String },
+  password: { type: String, required: true }
 })
+export const UserModel = mongoose.model('users', UserSchema)
--- server/routes/users.ts
+++ server/routes/users.ts
@@ -1,7 +1,14 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
+import { UserModel } from '../models/Users'
 
 const router = express.Router()
+
+router.post('/register', async (req, res) => {
+  const { username, password } = req.body
+  const user = await UserModel
+})
+router.get('/login')
 
 export { router as userRouter }
