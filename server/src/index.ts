cb9b005dd325a49bd62ebb47b6c41263f5802dc2
hamzatalha77 <hamza_ta7@hotmail.com>
2023-07-22 11:25:53 +0100

    commit

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,15 +2,18 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import mongoose from 'mongoose'
-
 import connectDB from '../config/database'
+import { userRouter } from '../routes/users'
 
 dotenv.config()
+
 const port = process.env.PORT
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+
+app.use('/auth', userRouter)
 
 app.get('/', (req, res) => {
   res.send('Express + TypeScript Server')
