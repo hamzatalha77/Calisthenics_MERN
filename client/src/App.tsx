8c95232b391effd0ee1b1680e7ef319a380fb62c
hamzatalha77 <hamza_ta7@hotmail.com>
2023-07-23 12:12:16 +0100

    commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import './App.css'
 const App = () => {
-  return <div></div>
+  return (
+    <div>
+      <h1 className="text-9xl text-red-600">hello</h1>
+    </div>
+  )
 }
 
 export default App
