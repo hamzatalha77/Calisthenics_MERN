4119bfbffbe3d02074f9c2ce9e627f5d08873487
hamzatalha77 <hamza_ta7@hotmail.com>
2023-07-23 11:24:46 +0100

    COMMIT

---
+++ server/src/controllers/userController.ts
@@ -0,0 +1,54 @@
+import { Request, Response } from 'express'
+import asyncHandler from 'express-async-handler'
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+import { UserModel } from '../models/Users'
+import generateToken from '../utils/generateToken'
+
+const LoginUser = asyncHandler(
+  async (req: Request, res: Response): Promise<void> => {
+    const { email, password } = req.body
+    const user = await UserModel.findOne({ email })
+
+    if (!user) {
+      res.status(400).json({ message: "User Doesn't Exist!" })
+      return
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+
+    if (!isPasswordValid) {
+      res.status(400).json({ message: 'Email or Password is Incorrect!' })
+      return
+    }
+
+    const token = generateToken(user._id)
+    res.status(201).json({ token, userID: user._id })
+  }
+)
+
+const registeredUser = asyncHandler(
+  async (req: Request, res: Response): Promise<void> => {
+    const { first_name, last_name, user_image, email, password } = req.body
+
+    const user = await UserModel.findOne({ email })
+
+    if (user) {
+      res.status(400).json({ message: 'User already exists!' })
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+    const newUser = new UserModel({
+      first_name,
+      last_name,
+      user_image,
+      email,
+      password: hashedPassword
+    })
+    await newUser.save()
+
+    res.status(201).json({ message: 'User Registered Successfully!' })
+  }
+)
+
+export { registeredUser, LoginUser }
--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -2,29 +2,12 @@ import express from 'express'
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
 import { UserModel } from '../models/Users'
+import { LoginUser, registeredUser } from '../controllers/userController'
 
 const router = express.Router()
 
-router.post('/register', async (req, res) => {
-  const { first_name, last_name, user_image, email, password } = req.body
+router.route('/register').post(registeredUser)
 
-  const user = await UserModel.findOne({ email })
-
-  if (user) {
-    return res.json({ message: 'User already exists!' })
-  }
-  const hashedPassword = await bcrypt.hash(password, 10)
-  const newUser = new UserModel({
-    first_name,
-    last_name,
-    user_image,
-    email,
-    password: hashedPassword
-  })
-  await newUser.save()
-
-  res.json({ message: 'User Registered Successfully !' })
-})
-router.post('/login')
+router.route('/login').post(LoginUser)
 
 export { router as userRouter }
