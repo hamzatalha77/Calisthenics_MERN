8828e8c8cb3a7a687fc4dd3079b954d3e46ed487
hamzatalha77 <hamza_ta7@hotmail.com>
2023-07-27 21:43:36 +0100

    commit

--- client/src/store.ts
+++ client/src/store.ts
@@ -1,2 +1,19 @@
-const store = () => {}
+import thunk from 'redux-thunk'
+import exerciseCreateReducers from './reducers/exerciseReducers'
+import { combineReducers, applyMiddleware, createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+const reducer = combineReducers({
+  exerciseCreate: exerciseCreateReducers
+})
+const initialState = {}
+
+const middleware = [thunk]
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+)
+
 export default store
