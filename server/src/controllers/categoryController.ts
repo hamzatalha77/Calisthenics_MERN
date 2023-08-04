d453ff6dd60f7c1de5d236f325699cfa017616ff
hamzatalha77 <hamza_ta7@hotmail.com>
2023-08-04 18:46:12 +0100

    commit

--- server/src/controllers/categoryController.ts
+++ server/src/controllers/categoryController.ts
@@ -38,4 +38,32 @@ const createCategory = async (req: Request, res: Response) => {
   }
 }
 
-export { getCategories, createCategory, uploadcategory }
+const updateCategory = async (req: Request, res: Response) => {
+  try {
+    const categoryId = req.params.id
+
+    const category = await CategoryModel.findById(categoryId)
+
+    if (!category) {
+      return res.status(404).json({ message: 'Category not found' })
+    }
+
+    uploadcategory.single('image_category')(req, res, async (err: any) => {
+      if (err) {
+        return res.status(400).json({ message: err.message })
+      }
+
+      category.category_name = req.body.category_name || category.category_name
+      category.slug = slugify(req.body.category_name || category.category_name)
+      if (req.file) {
+        category.image_category = req.file.path
+      }
+
+      const updatedCategory = await category.save()
+      res.status(200).json(updatedCategory)
+    })
+  } catch (error: any) {
+    res.status(400).json({ message: error.message })
+  }
+}
+export { getCategories, createCategory, uploadcategory, updateCategory }
--- server/src/routes/categories.ts
+++ server/src/routes/categories.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import {
   createCategory,
-  getCategories
+  getCategories,
+  updateCategory
 } from '../controllers/categoryController'
 
 const router = express.Router()
 
 router.route('/').post(createCategory).get(getCategories)
+router.route('/:id').put(updateCategory)
 
 export { router as categoryRouter }
