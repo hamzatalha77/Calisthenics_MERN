435d91def1cc0fa04dbd15182e60aa017fc6303a
hamzatalha77 <hamza_ta7@hotmail.com>
2023-08-05 11:21:07 +0100

    commit

---
+++ server/src/models/SubCategory.ts
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose'
+
+const subCategorySchema = new mongoose.Schema(
+  {
+    subcategory_name: {
+      type: String,
+      // trim: true,
+      unique: [true, 'SubCategory must be unique'],
+      minlength: [2, 'To short te be name'],
+      maxlength: [32, 'To long to be name']
+    },
+    slug: {
+      type: String,
+      lowercase: true
+    },
+    category: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'categories',
+      required: [true, 'Subcategory must be  belong to parent category']
+    }
+  },
+  { timestamps: true }
+)
+
+export const SubCategoryModel = mongoose.model('subcategory', subCategorySchema)
