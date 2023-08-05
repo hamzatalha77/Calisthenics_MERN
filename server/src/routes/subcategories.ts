fe199155f81cc569e7f234d05b272f2ab9c6b576
hamzatalha77 <hamza_ta7@hotmail.com>
2023-08-05 17:22:25 +0100

    commit

--- server/src/routes/subcategories.ts
+++ server/src/routes/subcategories.ts
@@ -1,8 +1,13 @@
 import express from 'express'
-import { createSubcategory } from '../controllers/subcategoryController'
+import {
+  createSubcategory,
+  getSubcategories,
+  getSubcategory
+} from '../controllers/subcategoryController'
 
 const router = express.Router()
 
-router.route('/').post(createSubcategory)
+router.route('/').post(createSubcategory).get(getSubcategories)
+router.route('/:id').get(getSubcategory)
 
 export { router as subcategoryRouter }
