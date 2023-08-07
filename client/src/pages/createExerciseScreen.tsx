3c98f8534a0a8475b9013d705fa591c98568b9af
hamzatalha77 <hamza_ta7@hotmail.com>
2023-08-07 13:35:29 +0100

    commit

--- client/src/pages/createExerciseScreen.tsx
+++ client/src/pages/createExerciseScreen.tsx
@@ -19,7 +19,7 @@ const CreateExerciseScreen = () => {
   const [sets, setSets] = useState<string>('')
   const [duration, setDuration] = useState<string>('')
   const [images, setImages] = useState<string[]>([])
-  const [category, setCatgory] = useState<string>('')
+  const [category, setCategory] = useState<string>('')
   const [imagesToUpload, setImagesToUpload] = useState<File[]>()
   const navigate = useNavigate()
   const dispatch = useDispatch<Dispatch<any>>()
@@ -154,7 +154,7 @@ const CreateExerciseScreen = () => {
           <select
             id="categories"
             value={category}
-            onChange={(e) => setCatgory(e.target.value)}
+            onChange={(e) => setCategory(e.target.value)}
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           >
             <option value="" disabled selected>
