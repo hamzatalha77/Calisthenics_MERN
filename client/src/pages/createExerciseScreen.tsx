e9fb379a9ab0b727539a3593b11d376277728eb9
hamzatalha77 <hamza_ta7@hotmail.com>
2023-08-07 13:38:25 +0100

    commit

--- client/src/pages/createExerciseScreen.tsx
+++ client/src/pages/createExerciseScreen.tsx
@@ -157,7 +157,7 @@ const CreateExerciseScreen = () => {
             onChange={(e) => setCategory(e.target.value)}
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           >
-            <option value="" disabled selected>
+            <option value="" disabled>
               Choose a category
             </option>
             {categories.map((category) => (
