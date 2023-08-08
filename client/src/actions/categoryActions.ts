38423102e1203381a0724b469d9a3a5c113ed967
hamzatalha77 <hamza_ta7@hotmail.com>
2023-08-08 20:14:16 +0100

    commit

--- client/src/actions/categoryActions.ts
+++ client/src/actions/categoryActions.ts
@@ -3,7 +3,9 @@ import {
   CATEGORY_LIST_REQUEST,
   CATEGORY_LIST_SUCCESS,
   CATEGORY_LIST_FAIL,
-  CATEGORY_CREATE_REQUEST
+  CATEGORY_CREATE_REQUEST,
+  CATEGORY_CREATE_SUCCESS,
+  CATEGORY_CREATE_FAIL
 } from '../constants/CategoryConstants'
 import { CategoryAction } from '../types/categoryTypes'
 
@@ -28,7 +30,21 @@ const createCategory =
   (category: any) => async (dispatch: (action: CategoryAction) => void) => {
     try {
       dispatch({ type: CATEGORY_CREATE_REQUEST })
-      const { data } = await axios.post('/api/categories', category)
-    } catch (error) {}
+      const config = {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      }
+      const { data } = await axios.post('/api/categories', category, config)
+      dispatch({ type: CATEGORY_CREATE_SUCCESS, payload: data })
+    } catch (error: any) {
+      dispatch({
+        type: CATEGORY_CREATE_FAIL,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message
+      })
+    }
   }
 export { listCategories, createCategory }
