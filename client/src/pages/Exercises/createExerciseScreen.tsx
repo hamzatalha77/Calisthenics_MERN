9ae69be321f0b5881f82f398f8205ca5c8ab18b5
hamzatalha77 <hamza_ta7@hotmail.com>
2023-08-11 16:45:12 +0100

    commit

--- client/src/pages/Exercises/createExerciseScreen.tsx
+++ client/src/pages/Exercises/createExerciseScreen.tsx
@@ -14,7 +14,7 @@ const CreateExerciseScreen = () => {
   const [video, setVideo] = useState<string>('')
   const [muscles, setMuscles] = useState<string>('')
   const [technique, setTechnique] = useState<string>('')
-  const [tags, setTags] = useState<string>('')
+  const [tags, setTags] = useState<string[]>([])
   const [reps, setReps] = useState<string>('')
   const [sets, setSets] = useState<string>('')
   const [duration, setDuration] = useState<string>('')
@@ -221,9 +221,14 @@ const CreateExerciseScreen = () => {
             <input
               type="text"
               id="tags"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              // ... other attributes ...
               placeholder="Tags"
-              onChange={(e) => setTags(e.target.value)}
+              onChange={(e) => {
+                const tagsArray = e.target.value
+                  .split(',')
+                  .map((tag) => tag.trim())
+                setTags(tagsArray)
+              }}
             />
           </div>
           <div>
